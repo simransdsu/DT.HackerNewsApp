75962ff5186477b384027a53db6749389dee8155
Simran Preet Singh <simran5590@gmail.com>
2022-06-23 08:19:46 -0700

    Showing articles in DetailView

--- HackerNewsApp/Views/ContentView.swift
+++ HackerNewsApp/Views/ContentView.swift
@@ -9,15 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
   
+  @StateObject var viewModel = ViewModel()
   
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    NavigationView {
+      List {
+        ForEach(viewModel.posts, id: \.objectID) { post in
+          NavigationLink {
+            DetailView(url: post.url)
+          } label: {
+            Text(post.title)
+              .font(.title3)
+              .padding(.vertical, 4)
+          }
+        }
+      }
+      .navigationTitle("Top News")
+    }
+    .task {
+      await viewModel.fetchPosts()
+    }
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
+  }
+}
+
+
+class ViewModel: ObservableObject {
+  @Published var posts = [Post]()
+  @Published var appError: AppError?
+  
+  func fetchPosts() async {
+    do {
+      posts = try await HackerNewsAPIService.shared.getPosts()
+    } catch {
+      if let error = error as? AppError {
+        appError = error
+        return
+      }
+      appError = AppError(message: "Something went wrong, please try again", description: error.localizedDescription)
+    }
   }
 }
