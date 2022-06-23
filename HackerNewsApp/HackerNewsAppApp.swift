b0273fcd209c3fd28dc7a5328297622dbd86b00b
Simran Preet Singh <simran5590@gmail.com>
2022-06-23 07:42:45 -0700

    Initial Commit

---
+++ HackerNewsApp/HackerNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsAppApp.swift
+//  HackerNewsApp
+//
+//  Created by Simran Preet Narang on 2022-06-23.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
